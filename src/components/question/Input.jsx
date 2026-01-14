4530b0fb008d5ee1287a5a6e60ffe4f51b287338
vobradovic <vobradovic17@gmail.com>
2026-01-14 16:43:36 +0100

    fix multiple submit answer when holding enter key

--- src/components/question/Input.jsx
+++ src/components/question/Input.jsx
@@ -1,8 +1,16 @@
+import { useRef } from "react";
+
 export default function Input({ playerWordInput, handlePlayerWordInput, handleCheck }) {
+    const eventTriggered = useRef(false);
+
     function handleEnterKey(event) {
-        if (event.key == 'Enter') {
-            handleCheck();
-        }
+      if (event.key == 'Enter' && !eventTriggered.current) {
+        eventTriggered.current = true;
+        handleCheck();
+        setTimeout(() => {
+          eventTriggered.current = false;
+        }, 1200);
+      }
     }
 
     return (
